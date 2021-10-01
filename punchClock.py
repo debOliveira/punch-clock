4016475064e1785fab9e694b2d9995c26aec4c61
Débora Oliveira <debora.oliveira@ee.ufcg.edu.br>
2021-10-01 17:31:49 -0300

    In registred for christian at 2021-10-01 - 17:31:48

--- punchClock.py
+++ punchClock.py
@@ -5,8 +5,10 @@
 from datetime import datetime
 import os
 from git import Repo
+import sys
 
-name = "name"
+name = sys.argv[1]
+mode = sys.argv[2]
 
 now = datetime.now()
 myfolder = os.path.abspath(os.getcwd())
@@ -14,11 +16,16 @@ myfolder = os.path.abspath(os.getcwd())
 
 current_time = now.strftime("%H:%M:%S")
 current_day = datetime.today().strftime('%Y-%m-%d')
-task = input("Enter what you've done: ")
-text = current_day+" - "+current_time+" >> "+task+"\n"
-msg = "Punch created for "+name+" at "+current_day+" - "+current_time
 
-f = open('./punchClock/'+name+'.txt','a')
+if mode == 'out':
+    task = input("Enter what you've done: ")
+    text = current_time+" >> "+task+"\n"
+    msg = "Punch created for "+name+" at "+current_day+" - "+current_time
+else:
+    text = current_day+" : "+current_time+" - "
+    msg = "In registred for "+name+" at "+current_day+" - "+current_time
+
+f = open('./punchClock/'+name+'.txt','a+')
 f.write(text)
 f.close()
 print(msg)
